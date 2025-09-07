732f165062c371b7ef221212b89de4b17b7d9a3a
prasiddhi <prasiddhiroy@gmail.com>
2025-09-07 09:16:42 +0530

    initial commit

---
+++ day1/day1_q1.c
@@ -0,0 +1,18 @@
+//prasiddhi
+#include <stdio.h>
+int main()
+{
+    // input two numbers
+    int a,b,sum=0;
+    printf("enter the 1st no.");
+    scanf("%d", &a);
+
+    printf("enter the 2nd no.");
+    scanf("%d", &b);
+    //calcute the sum of two numbers
+sum = a + b;
+//print the output
+printf("sum = %d\n", sum);
+
+return 0;
+}
