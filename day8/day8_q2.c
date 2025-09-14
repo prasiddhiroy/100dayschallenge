76fd8fc99102013bfa724d335807cfeb9769812b
prasiddhiroy <prasiddhiroy@gmail.com>
2025-09-15 00:12:38 +0530

    Update day8_q2.c

--- day8/day8_q2.c
+++ day8/day8_q2.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-int main() 
-{
+
+int main() {
     char ch;
 
     // Input
-    printf("Enter an alphabet: ");
+    printf("Enter a character: ");
     scanf("%c", &ch);
 
-    // Check if alphabet or not
-    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-        // Check vowel
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-            printf("%c is a Vowel.\n", ch);
-        } else {
-            printf("%c is a Consonant.\n", ch);
-        }
-    } else {
-        printf("Invalid input! Please enter an alphabet.\n");
+    // Check type of character
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("%c is an Uppercase Alphabet.\n", ch);
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("%c is a Lowercase Alphabet.\n", ch);
+    }
+    else if (ch >= '0' && ch <= '9') {
+        printf("%c is a Digit.\n", ch);
+    }
+    else {
+        printf("%c is a Special Character.\n", ch);
     }
 
     return 0;
