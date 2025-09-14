33b1b342a45a210722faaf89185bf3ed1488e5e7
prasiddhiroy <prasiddhiroy@gmail.com>
2025-09-14 23:52:27 +0530

    Add files via upload

---
+++ day6/day6_q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+
+    // Input
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Nested if–else
+    if (num >= 0) {
+        if (num == 0) {
+            printf("The number is Zero.\n");
+        } else {
+            printf("The number is Positive.\n");
+        }
+    } else {
+        printf("The number is Negative.\n");
+    }
+
+    return 0;
+}
