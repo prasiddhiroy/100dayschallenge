d7fee9cefe37e5e4f4864487b5d86c66ba08ee99
prasiddhiroy <prasiddhiroy@gmail.com>
2025-09-15 00:01:23 +0530

    Add files via upload

---
+++ day7/day7_q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+    int year;
+
+    // Input
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Leap year check
+    if (year % 400 == 0) {
+        printf("%d is a Leap Year.\n", year);
+    } else if (year % 100 == 0) {
+        printf("%d is Not a Leap Year.\n", year);
+    } else if (year % 4 == 0) {
+        printf("%d is a Leap Year.\n", year);
+    } else {
+        printf("%d is Not a Leap Year.\n", year);
+    }
+
+    return 0;
+}
